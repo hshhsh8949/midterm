987ecda70e9c882def191c2ccb35170b07d95677
hsh8949 <hshhsh8949@naver.com>
2023-04-20 21:45:19 +0900

    README, midterm
    세번쨰 commit

---
+++ midterm.r
@@ -0,0 +1,19 @@
+head(USArrests,3) 
+
+  foo=c(USArrests)
+  bar=c(,Assault)
+  
+  foobar <- data.frame(USArrests,Assault)
+
+  cbind(foo,bar)
+  
+  install.packages("wordcloud")
+  library(wordcloud)
+  
+  
+  
+  foobar <- read.xlsx('C:\midterm/foobar.xlsx'), header=T,
+  
+  
+  
+  rm(list = ls() ) 
